91433cc1fbbc305a8b3503f7116679d3a17f09f5
Matthew <matthew@codenow.org>
2013-07-09 16:50:06 -0400

    End of the day tuesday

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 require 'sinatra\reloader'
 
-secret_number = (1 + rand(99)).to_s
+@secret_number = (1 + rand(99))
 
-get "/" do
+get '/' do 
+  message = check_guess(@secret_number)
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+def number
+  @number ||= rand(100)
+end
+
+def check_guess
+  if params["guess"] >= @secret_number.to_s
+    puts "Too High!"
+  elsif params["guess"] <= @secret_number.to_s  	
+  	puts "Too Low!"
+  end
  erb :index
 end
